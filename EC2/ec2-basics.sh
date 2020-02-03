89c2b6cf08c2c011c2e755e4672721356fd6a45b
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-02-03 18:31:57 +0900

    learn: ELB and ASG

---
+++ EC2/ec2-basics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hell World from $(hostname -f)" > /var/www/html/index.html
